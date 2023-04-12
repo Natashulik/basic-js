6b463080064934986667fa53347a080e1f63238b
Natasha Victorovich <nata17v@mail.ru>
2023-04-12 23:21:58 +0300

    resolve task encode-line

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,10 +10,15 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+function encodeLine(str) {
+if(str==='') return '';
+  return str.split('').map((item,index,array)=>{
+     if(item !== array[index+1] && index !=array.length-1) {
+         return item +'!'
+     } else return item;
+  }).join('').split('!').map(item=> item.length+item[0]).join('').split('').filter(item => item !=1 && item !=0).join('')
+ 
+ }
 
 module.exports = {
   encodeLine
